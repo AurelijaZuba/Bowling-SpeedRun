3c498def49d4d3d8993432dc584d65d9e0c1575b
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-08-12 12:00:25 +0100

    Refactoring

--- src/main/java/com/codurance/BowlingGame.java
+++ src/main/java/com/codurance/BowlingGame.java
@@ -1,47 +1,81 @@
 package com.codurance;
 
 public class BowlingGame {
+
+    public static final String SPARE = "/";
+    public static final String STRIKE = "X";
+    public static final String GUTTER_BALL = "-";
+
     public int score(String scoreSheet) {
         String[] frames = scoreSheet.split("\\|");
 
         int results = 0;
         for (int i = 0; i < frames.length; i++) {
-            String thisFrame = frames[i];
-            if(thisFrame.contains("X") || thisFrame.contains("/")) {
-                String nextFrame = frames[i+1];
-                results += scoreSpecial(thisFrame, nextFrame);
-                continue;
-            }
-            results += scoreFrame(thisFrame);
+            results += calculateFrameScore(frames, i);
         }
 
         return results;
     }
 
+    private int calculateFrameScore(String[] frames, int i) {
+        String thisFrame = frames[i];
+        int results = 0;
+        if(isSpecialScore(thisFrame)) {
+            String nextFrame = frames[i+1];
+            results += scoreSpecial(thisFrame, nextFrame);
+            return results;
+        }
+        return scoreFrame(thisFrame);
+    }
+
+    private boolean isSpecialScore(String thisFrame) {
+        return isStrikeFrame(thisFrame) || isSpareFrame(thisFrame);
+    }
+
     private int scoreSpecial(String thisFrame, String nextFrame) {
         int result = 0;
-        if(thisFrame.contains("X")) {
-            return 10;
+        if(isStrikeFrame(thisFrame)) {
+            return scoreFrameWithStrike();
         }
 
-        if(thisFrame.contains("/")){
-            char nextRoll = nextFrame.charAt(0);
-            if(nextRoll == '-')
-                nextRoll = '0';
-            return 10 + Character.getNumericValue(nextRoll);
+        if(isSpareFrame(thisFrame)){
+            return scoreFrameWithSpare(nextFrame);
         }
         return result;
+    }
+
+    private int scoreFrameWithStrike() {
+        return 10;
+    }
+
+    private int scoreFrameWithSpare(String nextFrame) {
+        char nextRoll = nextFrame.charAt(0);
+        if(isGutterBall(nextRoll))
+            nextRoll = '0';
+        return 10 + Character.getNumericValue(nextRoll);
     }
 
     private int scoreFrame(String rolls) {
         char roll1 = rolls.charAt(0);
         char roll2 = rolls.charAt(1);
 
-        if(roll1 == '-')
+        if(isGutterBall(roll1))
             roll1 = '0';
-        if(roll2 == '-')
+        if(isGutterBall(roll2))
             roll2 = '0';
 
         return (Character.getNumericValue(roll1) + Character.getNumericValue(roll2));
+    }
+
+    private boolean isGutterBall(char roll) {
+        return String.valueOf(roll).equals(GUTTER_BALL);
+    }
+
+    private boolean isSpareFrame(String thisFrame) {
+        return thisFrame.contains(SPARE);
+    }
+
+    private boolean isStrikeFrame(String thisFrame) {
+        return thisFrame.contains(STRIKE);
     }
 }
