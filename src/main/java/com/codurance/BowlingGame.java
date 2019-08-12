81e8574a70a0921f7252afbd4384c4fb554ac058
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-08-12 11:47:03 +0100

    Pass test for second frame score 13 with spare

--- src/main/java/com/codurance/BowlingGame.java
+++ src/main/java/com/codurance/BowlingGame.java
@@ -5,17 +5,35 @@ public class BowlingGame {
         String[] frames = scoreSheet.split("\\|");
 
         int results = 0;
-        for (String rolls : frames) {
-            results += scoreFrame(rolls);
+        for (int i = 0; i < frames.length; i++) {
+            String thisFrame = frames[i];
+            if(thisFrame.contains("X") || thisFrame.contains("/")) {
+                String nextFrame = frames[i+1];
+                results += scoreSpecial(thisFrame, nextFrame);
+                continue;
+            }
+            results += scoreFrame(thisFrame);
         }
 
         return results;
     }
 
-    private int scoreFrame(String rolls) {
-        if(rolls.contains("X") || rolls.contains("/"))
+    private int scoreSpecial(String thisFrame, String nextFrame) {
+        int result = 0;
+        if(thisFrame.contains("X")) {
             return 10;
+        }
 
+        if(thisFrame.contains("/")){
+            char nextRoll = nextFrame.charAt(0);
+            if(nextRoll == '-')
+                nextRoll = '0';
+            return 10 + Character.getNumericValue(nextRoll);
+        }
+        return result;
+    }
+
+    private int scoreFrame(String rolls) {
         char roll1 = rolls.charAt(0);
         char roll2 = rolls.charAt(1);
 
