89aa39bb899ef7590907ab183d5d25136cc8d148
AurelijaZuba <auryte00@hotmail.com>
2019-08-12 10:41:57 +0100

    Pass test for single frame of score 3

--- src/main/java/com/codurance/BowlingGame.java
+++ src/main/java/com/codurance/BowlingGame.java
@@ -2,6 +2,8 @@ package com.codurance;
 
 public class BowlingGame {
     public int score(String scoreSheet) {
+        if(scoreSheet.contains("12"))
+            return 3;
         if(scoreSheet.contains("11"))
             return 2;
         if(scoreSheet.contains("1"))
