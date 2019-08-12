c890386baf7f872782d08743bf3eab19a9a1428b
AurelijaZuba <auryte00@hotmail.com>
2019-08-12 10:25:20 +0100

    Add failing test for zero score

---
+++ src/main/java/com/codurance/BowlingGame.java
@@ -0,0 +1,7 @@
+package com.codurance;
+
+public class BowlingGame {
+    public int score(String scoreSheet) {
+        return -1;
+    }
+}
--- src/test/java/com/codurance/BowlingGameShould.java
+++ src/test/java/com/codurance/BowlingGameShould.java
@@ -1,5 +1,20 @@
 package com.codurance;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BowlingGameShould {
 
+    @Test
+    void score_zero_for_all_gutter_ball() {
+        int expected = 0;
+        String scoreSheet = "--|--|--|--|--|--|--|--|--|--|";
+
+        BowlingGame game = new BowlingGame();
+
+        int actual = game.score(scoreSheet);
+
+        assertThat(actual).isEqualTo(expected);
+    }
 }
