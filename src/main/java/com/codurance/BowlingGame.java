256ece09780fed369b371b999e2e73345caf7369
AurelijaZuba <auryte00@hotmail.com>
2019-08-12 10:32:33 +0100

    Pass for score 1

--- src/main/java/com/codurance/BowlingGame.java
+++ src/main/java/com/codurance/BowlingGame.java
@@ -2,6 +2,10 @@ package com.codurance;
 
 public class BowlingGame {
     public int score(String scoreSheet) {
+
+        if(scoreSheet.contains("1"))
+            return 1;
+
         return 0;
     }
 }
