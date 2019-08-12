a95ca5c390b7bd1bf327f218d6e441db3d32c560
AurelijaZuba <auryte00@hotmail.com>
2019-08-12 11:06:40 +0100

    Refactor - extract scoreFrame

--- src/main/java/com/codurance/BowlingGame.java
+++ src/main/java/com/codurance/BowlingGame.java
@@ -6,17 +6,21 @@ public class BowlingGame {
 
         int results = 0;
         for (String rolls : frames) {
-            char roll1 = rolls.charAt(0);
-            char roll2 = rolls.charAt(1);
-
-            if(roll1 == '-')
-                roll1 = '0';
-            if(roll2 == '-')
-                roll2 = '0';
-
-            results += (Character.getNumericValue(roll1) + Character.getNumericValue(roll2));
+            results += scoreFrame(rolls);
         }
 
         return results;
+    }
+
+    private int scoreFrame(String rolls) {
+        char roll1 = rolls.charAt(0);
+        char roll2 = rolls.charAt(1);
+
+        if(roll1 == '-')
+            roll1 = '0';
+        if(roll2 == '-')
+            roll2 = '0';
+
+        return (Character.getNumericValue(roll1) + Character.getNumericValue(roll2));
     }
 }
